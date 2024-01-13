8f5a755f5bbe960f3e1ff31f328b540856854ce4
eyhdev <alaqsa335@gmail.com>
2024-01-13 14:33:12 +0300

    Initial Commit

---
+++ Quran.App/Quran_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quran_AppApp.swift
+//  Quran.App
+//
+//  Created by eyh.mac on 13.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Quran_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
